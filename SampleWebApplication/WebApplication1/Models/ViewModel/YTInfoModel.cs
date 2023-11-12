5f26cb38f99f6dd47b121a745b1c56bb602988a4
ldlaigo <119918582+ldlaigo@users.noreply.github.com>
2023-11-12 23:21:15 +0800

    Additional RegEx

--- SampleWebApplication/WebApplication1/Models/ViewModel/YTInfoModel.cs
+++ SampleWebApplication/WebApplication1/Models/ViewModel/YTInfoModel.cs
@@ -7,8 +7,16 @@ namespace MyWebApplication.Models.ViewModel
         [Key]
         public int id { get; set; }
         [Required(ErrorMessage = "*")]
+        [RegularExpression(@"^(https?://)?(www\.)?(youtube\.com/(\S+/?\S*|embed/|v/|channels/|user/\S+)|youtu\.be/(\S+/?\S*))$", ErrorMessage = "Invalid Youtube Link")]
+        [Display(Name = "YTLink")]
         public string YTLink { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(100, ErrorMessage = "Maximum length is 100 characters.")]
+        [Display(Name = "YTTitle")]
         public string YTTitle { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(55, ErrorMessage = "Maximum length is 55 characters.")]
+        [Display(Name = "YTTitle")]
         public string Notes { get; set; }
         public int CreatedBy { get; set; }
     }
